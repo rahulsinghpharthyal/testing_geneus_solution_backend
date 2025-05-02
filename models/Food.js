c5b88ab636797d7f9b04437fdb1716f17de368c0
EternalSovereign <dipendrakumarbm5@gmail.com>
2025-05-02 09:14:28 +0530

    Mongo Db Model issue

--- models/Food.js
+++ models/Food.js
@@ -3,47 +3,46 @@ import mongoose from "mongoose";
 const foodSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: "User",
     },
     breakfast: [
         {
             item: {
                 type: mongoose.Schema.Types.ObjectId,
-                ref: "Item"
+                ref: "FoodItem",
             },
             quantity: {
                 type: Number,
-                default: 1
-            }
-        }
+                default: 1,
+            },
+        },
     ],
     lunch: [
         {
             item: {
                 type: mongoose.Schema.Types.ObjectId,
-                ref: "Item"
+                ref: "FoodItem",
             },
             quantity: {
                 type: Number,
-                default: 1
-            }
-        }
+                default: 1,
+            },
+        },
     ],
     dinner: [
         {
             item: {
                 type: mongoose.Schema.Types.ObjectId,
-                ref: "Item"
+                ref: "FoodItem",
             },
             quantity: {
                 type: Number,
-                default: 1
-            }
-        }
-    ]
+                default: 1,
+            },
+        },
+    ],
 });
 
-
-const Food = mongoose.model('UserDietDiary', foodSchema);
+const Food = mongoose.model("UserDietDiary", foodSchema);
 
 export default Food;
